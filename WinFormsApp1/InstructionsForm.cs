5f874e549f3d9d240c3d9aab9fda0be2c1b0f68f
Umer <umernoor6660@gmail.com>
2024-11-20 16:42:37 +0500

    instruction form

--- WinFormsApp1/InstructionsForm.cs
+++ WinFormsApp1/InstructionsForm.cs
@@ -26,7 +26,7 @@ namespace WinFormsApp1
                 Label instructionsLabel = new Label
                 {
                     Text = "This is a time-limited test. You will have 57 minutes to complete each question.\n\n" +
-                           "Once you start the quiz, the timer will begin, and your result will be recorded as your real attempt.\n\n" +
+                           "Once you start the quiz, the timer will begin, and your result will be recorded as your real attempt.You cant quit to the main menu\n\n" +
                            "Please read the instructions carefully before starting.",
                     AutoSize = true,
                     Location = new Point(20, 20),
